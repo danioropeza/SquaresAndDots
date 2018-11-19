626a4afcf8703f526b9e5ca7fdc873b7ba7687ce
Julia1297 <valita129@gmail.com>
2018-11-18 20:44:04 -0400

    Reiniciar partida

--- features/step_definitions/reiniciarPartida_spec.rb
+++ features/step_definitions/reiniciarPartida_spec.rb
@@ -1,3 +1,4 @@
 Then("mi puntaje deberia ser {int}") do |puntaje|
-    expect(page).to have_content(puntaje.to_s + "\r\n\r\nJohn" )
+   # expect(page).to have_content(puntaje.to_s + "\r\n\r\nJohn" )
+   expect(page).to have_content(puntaje)
 end
